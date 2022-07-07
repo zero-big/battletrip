6a18ada7c96d62ab3a267757b7f41afbbdd6f5ab
zerobig <zerobiig@gmai.com>
2022-07-07 12:30:32 +0900

    new crawling

--- job01_crawling.py
+++ job01_crawling.py
@@ -14,26 +14,32 @@ city = ['중국','일본', '베트남', '태국', '필리핀',
         '북유럽','터키','스페인','미서부','미동부',
         '중남미','시드니','뉴질랜드','아프리카', '이집트']
 
-url = 'https://www.ybtour.co.kr/eplg/episodeList.yb?pageNo=&bestYn=&writeDiviCd=&subDspMenu=&srchParam=&srchParamContent=&searchCnd=ALL&searchWrd={}.format(list[1])
-for i in range(1, 451):
-    url = 'https://www.ybtour.co.kr/eplg/episodeList.yb?pageNo={}'.format(list[0])
-    titles = []
-    reviews = []
-    for j in range(1, 21):
-        back_flag = False
-        driver.get(url)
-        time.sleep(1.0)
-        title = driver.find_element("xpath", '//*[@id="content"]/table/tbody/tr[{}]/td[4]/a'.format(j)).text
-        # print(title)
-        review = driver.find_element('xpath', '//*[@id="content"]/table/tbody/tr[{}]/td[4]/a'.format(j)).click()
-        time.sleep(1.0)
-        review_page = driver.find_element('xpath', review_page_xpath).text
-        # print(review_page)
-        titles.append(title)
-        reviews.append(review_page)
-        driver.back()
-        time.sleep(1.0)
-        print(i, j)
+url = 'https://www.ybtour.co.kr/search/searchPdt.yb?query={}&departDate=&cityList={}'.format(city[0],city[0])
+driver.get(url)
+time.sleep(1.0)
+# time.sleep(1.0)
+titles = []
+reviews = []
+driver.find_element('xpath', '//*[@id="container"]/div[2]/div[3]/ul/li[3]/a').click()
+time.sleep(1.0)
+for i in range(1, 405):
+    # back_flag = False
+
+    # time.sleep(1.0)
+    title = driver.find_element("xpath", '//*[@id="tab_page"]/div[4]/table/tbody/tr[{}]/td[4]/a'.format(i)).text
+    time.sleep(1.0)
+    print(title)
+    review = driver.find_element('xpath', '//*[@id="tab_page"]/div[4]/table/tbody/tr[{}]/td[4]/a'.format(i)).click()
+    time.sleep(1.0)
+    review_page = driver.find_element('xpath', review_page_xpath).text
+    print(review_page)
+    titles.append(title)
+    reviews.append(review_page)
+    driver.back()
+    time.sleep(1.0)
+    driver.find_element('xpath', '//*[@id="container"]/div[2]/div[3]/ul/li[3]/a').click()
+    time.sleep(1.0)
+    print(i)
     df = pd.DataFrame({'title': titles, 'reviews': reviews})
     df.to_csv('./crawling_data/reviews_{}page.csv'.format(i), index=False)
 driver.close()
