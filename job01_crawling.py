76ff85cafe108e7361a6f07c0d4b74b5bfa9e5be
zerobig <zerobiig@gmai.com>
2022-07-07 12:58:55 +0900

    new crawling

--- job01_crawling.py
+++ job01_crawling.py
@@ -14,32 +14,44 @@ city = ['중국','일본', '베트남', '태국', '필리핀',
         '북유럽','터키','스페인','미서부','미동부',
         '중남미','시드니','뉴질랜드','아프리카', '이집트']
 
-url = 'https://www.ybtour.co.kr/search/searchPdt.yb?query={}&departDate=&cityList={}'.format(city[0],city[0])
-driver.get(url)
-time.sleep(1.0)
+# review_range = driver.find_element('xpath', '//*[@id="tab_page"]/div[4]/table/tbody/tr[1]/td[1]').text
+# review_range = review_range.replace(',', '')
+# review_range = int(review_range) + 1
+for j in city[0:5]:
+    url = 'https://www.ybtour.co.kr/search/searchPdt.yb?query={}&departDate=&cityList={}'.format(j,j)
+    driver.get(url)
+    time.sleep(2.0)
 # time.sleep(1.0)
-titles = []
-reviews = []
-driver.find_element('xpath', '//*[@id="container"]/div[2]/div[3]/ul/li[3]/a').click()
-time.sleep(1.0)
-for i in range(1, 405):
-    # back_flag = False
-
-    # time.sleep(1.0)
-    title = driver.find_element("xpath", '//*[@id="tab_page"]/div[4]/table/tbody/tr[{}]/td[4]/a'.format(i)).text
-    time.sleep(1.0)
-    print(title)
-    review = driver.find_element('xpath', '//*[@id="tab_page"]/div[4]/table/tbody/tr[{}]/td[4]/a'.format(i)).click()
-    time.sleep(1.0)
-    review_page = driver.find_element('xpath', review_page_xpath).text
-    print(review_page)
-    titles.append(title)
-    reviews.append(review_page)
-    driver.back()
-    time.sleep(1.0)
+    titles = []
+    reviews = []
     driver.find_element('xpath', '//*[@id="container"]/div[2]/div[3]/ul/li[3]/a').click()
-    time.sleep(1.0)
-    print(i)
-    df = pd.DataFrame({'title': titles, 'reviews': reviews})
+    review_range = driver.find_element('xpath', '//*[@id="tab_page"]/div[4]/table/tbody/tr[1]/td[1]').text
+    review_range = review_range.replace(',', '')
+    review_range = int(review_range) + 1
+    time.sleep(2.0)
+    print(review_range)
+    for i in range(1, review_range):
+        # back_flag = False
+
+        # time.sleep(1.0)
+        title = driver.find_element("xpath", '//*[@id="tab_page"]/div[4]/table/tbody/tr[{}]/td[4]/a'.format(i)).text
+        time.sleep(3.0)
+        print(title)
+        review = driver.find_element('xpath', '//*[@id="tab_page"]/div[4]/table/tbody/tr[{}]/td[4]/a'.format(i)).click()
+        time.sleep(3.0)
+        review_page = driver.find_element('xpath', review_page_xpath).text
+        print(review_page)
+        titles.append(title)
+        reviews.append(review_page)
+        driver.back()
+        time.sleep(3.0)
+        driver.find_element('xpath', '//*[@id="container"]/div[2]/div[3]/ul/li[3]/a').click()
+        time.sleep(3.0)
+        print(i)
+        df = pd.DataFrame({'title': titles, 'reviews': reviews})
+        if i // 20 == 0:
+            df.to_csv('./crawling_data/reviews_{}page.csv'.format(i), index=False)
+        else:
+            pass
     df.to_csv('./crawling_data/reviews_{}page.csv'.format(i), index=False)
 driver.close()
