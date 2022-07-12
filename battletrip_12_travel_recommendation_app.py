138a93729b04ea6e9834afe9802e422dbef8c984
xlomin8 <liberosm0818@naver.com>
2022-07-13 03:45:22 +0900

    travel recommendation app

--- battletrip_12_travel_recommendation_app.py
+++ battletrip_12_travel_recommendation_app.py
@@ -18,11 +18,14 @@ class Exam(QWidget, form_window):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.Tfidf_matrix = mmread('./models/Tfidf_trip_review.mtx').tocsr()
 
+        # 모델 로드
+        self.Tfidf_matrix = mmread('./models/Tfidf_trip_review.mtx').tocsr()
         with open('./models/tfidf.pickle', 'rb') as f:
             self.Tfidf = pickle.load(f)
         self.embedding_model = Word2Vec.load('./models/word2vec_battletrip.model')
+
+        # 콤보박스에 국가 리스트 추가
         # self.comboBox.addItem('2017~2022 영화 리스트')
         self.df_reviews = pd.read_csv('./crawling_data/every_country_reviews.csv')
         self.country = list(self.df_reviews['country'])
@@ -33,9 +36,12 @@ class Exam(QWidget, form_window):
         # exit()
         # for title in self.country:
         #     self.comboBox.addItem(title)
+
+        # 콤보박스 클릭 시
         self.cmb_region.currentIndexChanged.connect(self.cmb_region_slot)
         self.cmb_who.currentIndexChanged.connect(self.cmb_who_slot)
         self.cmb_purpose.currentIndexChanged.connect(self.cmb_purpose_slot)
+        # 추천 버튼 클릭 시
         self.btn_recommendation.clicked.connect(self.btn_slot)
 
     def getRecommendation(self, cosine_sim):
@@ -74,13 +80,16 @@ class Exam(QWidget, form_window):
 
     def btn_slot(self):
         keywords = []
+        # 사용자가 '국가' 콤보박스 지정
         if self.cmb_region.currentText():
             keywords.append(self.cmb_region.currentText())
-
+        # 사용자가 '테마' 콤보박스 지정
         if self.cmb_purpose.currentText():
             keywords.append(self.cmb_purpose.currentText())
+        # 사용자가 '일행' 콤보박스 지정
         if self.cmb_who.currentText():
             keywords.append(self.cmb_who.currentText())
+        #
         if self.le_recommendation.text():
             keywords.append(self.le_recommendation.text())
         print(keywords)
@@ -108,13 +117,16 @@ class Exam(QWidget, form_window):
         self.lbl_recommendation3.setPixmap(pixmap[2])
 
 
+    # 키워드로 추천하는 함수
     def recommendation_by_keyword(self, keyword):
+        # 사용자가 키워드 입력 시
         if keyword:
             try:
                 keyword = keyword.split()
                 lb_keyword = keyword[3]
                 print(lb_keyword)
                 sim_word = self.embedding_model.wv.most_similar(lb_keyword, topn=10)
+                # 유사도 값 큰 순서로 7개 단어 리스트
                 words = [lb_keyword]
                 for word, _ in sim_word:
                     words.append(word)
@@ -134,19 +146,24 @@ class Exam(QWidget, form_window):
                 cleaned_sentence = ' '.join(cleaned_sentence)
                 print(cleaned_sentence)
                 print('debug02')
+                # 모델 적용
                 sentence_vec = self.Tfidf.transform([cleaned_sentence])
                 print(sentence_vec)
+                # 코사인 유사도
                 cosine_sim = linear_kernel(sentence_vec, self.Tfidf_matrix)
                 print('debug05')
-                recommendation = self.getRecommendation(cosine_sim)
+                # 추천
+                recommendation = self.getRecommendation(cosine_sim) # 리스트 형태
                 print(recommendation)
 
                 print('debug03')
-                recommendation = '\n'.join(list(recommendation[:3]))
+                recommendation = '\n'.join(list(recommendation[:3]))    # 문자열(줄바꿈) 형태
                 return recommendation
+            # 명사, 형용사/부사, 숫자 아닌 키워드 입력 시
             except:
                 self.lbl_recommendation.setText("알 수 없는 단어입니다.")
                 return 0
+        # 사용자가 키워드 입력하지 않고 추천 버튼만 누른 경우
         else:
             return 0
 
